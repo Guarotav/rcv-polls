b6a2386e5d424c4a257043190af6a885ed8a0fc5
Guarotav <guarotav@gmail.com>
2026-01-05 18:44:57 -0500

    Add project rule and navbar login behavior
    Add a project rule to build and test when project files change.
    Update navbar so that when a user is logged in, it only displays
    the logout option and the logged-in username.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,22 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBarStyles.css";
 
-const NavBar = () => {
+const NavBar = ({ user, onLogout }) => {
   return (
     <nav className="navbar">
       <div className="nav-brand">
-        <Link to="/">Rankly</Link>
+        <Link to="/">Ranked Voting</Link>
       </div>
 
       <div className="nav-links">
-        <div className="auth-links">
-          <Link to="/login" className="nav-link">
-            Login
-          </Link>
-          <Link to="/signup" className="nav-link">
-            Sign Up
-          </Link>
-        </div>
+        {user ? (
+          <div className="user-section">
+            <span className="username">{user.username}</span>
+            <button onClick={onLogout} className="logout-btn">
+              Log out
+            </button>
+          </div>
+        ) : (
+          <div className="auth-links">
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+            <Link to="/signup" className="nav-link">
+              Sign Up
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
